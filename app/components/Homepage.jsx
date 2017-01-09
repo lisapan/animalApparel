439f6f65671c67e3ddb2124144e3523c797806aa
Kate <katelynj29@gmail.com>
2017-01-09 18:24:24 -0500

    added component files for the homepage

---
+++ app/components/Homepage.jsx
@@ -0,0 +1,29 @@
+'use strict'
+
+import React from 'react'
+
+export default props => {
+  return (
+    <div className="container-fluid" >
+      <div className="row">
+        <div className="col-md-12">
+          {/*promo*/}
+          <h1>Hi!</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+          {/*women*/}
+        </div>
+        <div className="col-md-6">
+          {/*men*/}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          {/*dogs*/}
+        </div>
+      </div>
+    </div>
+  );
+}
