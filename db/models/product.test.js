9e4d6b405851bab775593d475ea4415e0ae7f243
Chloe Rice <chloe.mi.rice@gmail.com>
2017-01-10 12:08:29 -0500

    removed size and inventory from product model test, they will be columns on the inventory model

--- db/models/product.test.js
+++ db/models/product.test.js
@@ -12,21 +12,17 @@ describe('Product', () => {
    name: 'Pokemon Dog Costume',
    description: 'Cute, soft, yellow one piece Pikachu costume for your pup.',
    price: '30.00',
-   size: ['XS', 'S', 'M', 'L', 'XL'],
-   inventory: [30, 19, 31, 0, 1],
    imageURL: 'wYu7cQs.jpg',
    tags: ['pokemon', 'costume', 'halloween', 'pikachu', 'dog']
   }
 
   describe('Created product successfully added to database', () => {
-    it('has a name, description, price, size, inventory, image url, and tags', () =>
+    it('has a name, description, price, image url, and tags', () =>
       Product.create(dogPokemonSuit)
         .then(createdProduct => {
           expect(createdProduct.name).to.equal(dogPokemonSuit.name)
           expect(createdProduct.description).to.equal(dogPokemonSuit.description)
           expect(createdProduct.price).to.equal(dogPokemonSuit.price)
-          expect(createdProduct.size).to.deep.equal(dogPokemonSuit.size)
-          expect(createdProduct.inventory).to.deep.equal(dogPokemonSuit.inventory)
           expect(createdProduct.imageURL).to.equal(dogPokemonSuit.imageURL)
           expect(createdProduct.tags).to.deep.equal(dogPokemonSuit.tags)
         })
