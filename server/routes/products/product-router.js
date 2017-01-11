9a640e80d779ce3487b3186b6dd8fd865fb1e039
Lisa Pan <l.pan4952@gmail.com>
2017-01-11 18:40:20 -0500

    sidebar and working on grabbing the correctly filtered products

--- server/routes/products/product-router.js
+++ server/routes/products/product-router.js
@@ -29,7 +29,7 @@ module.exports = require('express').Router()
 		.catch(next)
 	})
 	.get('/:tag', (req,res,next) => {
-		Product.findByTag([req.params.tag], {include: [Inventory]})
+		Product.findByTag(req.params.tag, {include: [Inventory]})
 		.then(products => {
 			res.json(products)
 		})
