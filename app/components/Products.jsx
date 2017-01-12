f10eec171ab6ea886f1444cd5f4b804a45d6095c
Chloe Rice <chloe.mi.rice@gmail.com>
2017-01-12 02:48:18 -0500

    fixed typos and missing image

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,36 +1,44 @@
-import React from 'react';
-import {Link} from 'react-router';
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 
 import Sidebar from './Sidebar';
 
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Thumbnail } from 'react-bootstrap';
 
-export default props => {
+const Products = (props) => {
 
   return (
     <Grid fluid={true}>
-      <Col md={3}>
-        <Sidebar />
-      </Col>
-      <Col md={9}>
-        <h3>Products</h3>
-        <div className="row">
-          {
-            props.products.map(product => (
-              <div className="col-xs-4" key={ product.id }>
-                <Link className="thumbnail" to={`/products/${product.id}`}>
-                  <img src={ product.imageUrl }/>
-                  <div className="caption">
-                    <h5>
-                      <span>{ product.name }</span>
-                    </h5>
-                  </div>
-                </Link>
-              </div>
-            ))
-          }
-        </div>
-      </Col>
+      <Row>
+        <Col md={3}>
+          <Sidebar />
+        </Col>
+        <Col md={9}>
+          <h3>Products</h3>
+          <div className="row">
+            {
+              props.products.map(product => (
+                <div className="col-xs-4" key={ product.id }>
+                  <Link to={`/products/${product.id}`}>
+                    <Thumbnail src={ product.imageURL } alt={`${product.name} photo`}/>
+                    <div className="caption">
+                      <h5>
+                        <span>{ product.name }</span>
+                      </h5>
+                    </div>
+                  </Link>
+                </div>
+              ))
+            }
+          </div>
+        </Col>
+      </Row>
     </Grid>
   );
 };
+
+Products.propTypes = {
+  products: PropTypes.array.isRequired,
+}
+
+export default Products
