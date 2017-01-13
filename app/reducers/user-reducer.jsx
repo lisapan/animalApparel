0941573618b42e1622ee64bc0725f531a449b170
Lisa Pan <l.pan4952@gmail.com>
2017-01-13 11:19:35 -0500

    user action creators reducer and dispatchers set up

--- app/reducers/user-reducer.jsx
+++ app/reducers/user-reducer.jsx
@@ -1,19 +1,45 @@
-const initialUserState = {
-  username: '',
-  password: '',
-};
+import axios from 'axios';
 
-export default function (state = initialUserState, action) {
+/* -----------------    ACTIONS     ------------------ */
 
-  const newState = Object.assign({}, state);
+const INITIALIZE    = 'INITIALIZE_USERS';
+const CREATE        = 'CREATE_USER';
 
+
+/* ------------   ACTION CREATORS     ------------------ */
+
+const init   = users => ({ type: INITIALIZE, users });
+export const create = user => ({ type: CREATE, user });
+
+
+
+/* ------------       REDUCER     ------------------ */
+
+export default function reducer (users = [], action) {
   switch (action.type) {
 
+    case INITIALIZE:
+      return action.users;
+
+    case CREATE:
+      return [action.user, ...users];
+
     default:
-      return state;
-
+      return users;
   }
-
-  return newState;
-
 }
+
+
+/* ------------       DISPATCHERS     ------------------ */
+
+export const fetchUsers = () => dispatch => {
+  axios.get('/api/users')
+       .then(res => dispatch(init(res.data)));
+};
+
+export const addUser = user => dispatch => {
+  axios.post('/api/users', user)
+       .then(res => dispatch(create(res.data)))
+       .catch(err => console.error(`Creating user: ${user} unsuccesful`, err));
+};
+
