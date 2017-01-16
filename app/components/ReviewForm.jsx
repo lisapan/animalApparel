b8d8b4a12e36dd0c159d15404c7cb1eb602a3d37
Kate <katelynj29@gmail.com>
2017-01-16 18:15:14 -0500

    minor syntax updates

---
+++ app/components/ReviewForm.jsx
@@ -0,0 +1,6 @@
+'use strict'
+
+import React, { Component } from 'react'
+import StarRating from 'react-star-rating'
+import { field, reduxForm } from 'redux-form'
+
---
+++ server/routes/review-router.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const db = require('APP/db')
+const Review = db.model('reviews')
+
+module.exports = require('express').Router()
+  //User posts new review for a product
+  .post('/', (req, res, send) => {
+    Review.create(req.body)
+      .then(review => res.status(201).json(review))
+      .catch(next)
+  })
+  //User(from account view) views all of their past reviews
+  .get('/:userId', (req, res, next) => {
+    Review.findAll({ where: { user_id: req.params.userId } })
+      .then(foundReviews => res.json(foundReviews))
+      .catch(next)
+  })
+  //User(from account view) deletes their past review
+  .delete('/:reviewId', (req, res, next) => {
+    Review.findById(req.params.reviewId)
+      .then(foundReview => res.status(204).json(foundReview))
+      .catch(next)
+  })
