0144cf6f531d96befc5bcdf71871e6cd2c4d7a8b
Chloe Rice <chloe.mi.rice@gmail.com>
2017-03-07 22:37:18 -0500

    fixed page reload on related product click

--- app/components/AllProducts.jsx
+++ app/components/AllProducts.jsx
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react'
-import { Link } from 'react-router'
+import { LinkContainer } from 'react-router-bootstrap'
 import { connect } from 'react-redux'
 
 import { Grid, Row, Col, Thumbnail } from 'react-bootstrap'
@@ -15,12 +15,12 @@ const Products = props => (
                 className="product"
                 key={ product.id }
                 xs={12} sm={6} md={4} lg={4}>
-                <Link to={`/products/${props.params.category}/${product.id}`}>
+                <LinkContainer to={{pathname: `/products/${props.params.category}/${product.id}`}}>
                   <Thumbnail src={product.image_URL} alt={`${product.name} photo`}>
                   <h5>{product.name}</h5>
                   <p>{`$${product.price}`}</p>
                   </Thumbnail>
-                </Link>
+                </LinkContainer>
               </Col>
             )) }
         </Row>
--- app/components/RelatedProducts.jsx
+++ app/components/RelatedProducts.jsx
@@ -1,18 +1,22 @@
+'use strict'
 
 import React, { PropTypes } from 'react'
 import { Row, Col, Thumbnail } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const RelatedProducts = props => (
   <Row className="product-detail">
     <h3>You might also like</h3>
     { props.relatedProducts.map(prod => (
         <Col className="product" key={ prod.id } xs={12} sm={6} md={3} lg={3}>
-          <Thumbnail
-            href={`/products/${prod.category}/${prod.id}`}
-            src={prod.image_URL} alt={`${prod.name} photo`}>
-            <h5>{prod.name}</h5>
-            <p>{`$${prod.price}`}</p>
-          </Thumbnail>
+          <LinkContainer to={{pathname: `/products/${prod.category}/${prod.id}`}}>
+            <Thumbnail
+              href={`/products/${prod.category}/${prod.id}`}
+              src={prod.image_URL} alt={`${prod.name} photo`}>
+              <h5>{prod.name}</h5>
+              <p>{`$${prod.price}`}</p>
+            </Thumbnail>
+          </LinkContainer>
         </Col>
       )) }
   </Row>
