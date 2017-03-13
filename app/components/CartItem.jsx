ed42f9ca2e3111021c02f745617669baf40ffd8a
Chloe Rice <chloe.mi.rice@gmail.com>
2017-03-12 22:07:24 -0400

    updated cart product links

--- app/components/CartItem.jsx
+++ app/components/CartItem.jsx
@@ -1,22 +1,24 @@
+'use strict'
+
 import React, { PropTypes } from 'react'
 import { Button, Image, Glyphicon } from 'react-bootstrap'
 
-const CartItem = (props) => (
+const CartItem = props => (
   <tr key={props.item.id}>
     <td>
       <Image
         className="cart-props.item"
         src={props.item.product.image_URL}
         alt={`${props.item.product.name} photo`}
-        href={`/products/product/${props.item.product.id}`}/>
+        href={`/products/${props.item.category}/${props.item.product.id}`} />
     </td>
     <td>
       <h4>{props.item.product.name}</h4>
       <Button bsStyle="primary" className="cart-props.item-button">
-        <Glyphicon glyph="pencil"/>
+        <Glyphicon glyph="pencil" />
       </Button>
       <Button bsStyle="primary" className="cart-props.item-button">
-        <Glyphicon glyph="trash"/>
+        <Glyphicon glyph="trash" />
       </Button>
     </td>
     <td>{props.item.size}</td>
